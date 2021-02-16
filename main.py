27153c0c94815798f9c0d84256bdbbefb1b5ece0
Nirogu <nirogu@outlook.com>
2021-02-16 13:23:29 -0500

    Corrige regiones y puntaje

--- main.py
+++ main.py
@@ -13,17 +13,20 @@ print('Done!')
 
 # Create experiment
 p = 0.6 # threshold
-Pl = 5 # number of players
-rounds = 60 # number of rounds
-groups = 50 # number of groups
+Pl = 2 # number of players
+rounds = 10 # number of rounds
+groups = 1 # number of groups
 gameParameters = [p, Pl, rounds, groups]
 measures = '13'
 TO_FILE = True
-non_shaky_hand = 0.88
 
-playerParameters1 =  {'ALL': 0.1} # Bias towards ALL
-playerParameters1['NOTHING'] = 0.1 # Bias towards NOTHING
-playerParameters1['3-GO-2-STAY'] = 0.1 # Bias towards 3-GO-2-STAY
+playerParameters1 =  {'ALL': 0.05} # Bias towards ALL
+playerParameters1['NOTHING'] = 0.05 # Bias towards NOTHING
+playerParameters1['ALTER1'] = 0.05 # Bias towards 3-GO-2-STAY
+playerParameters1['ALTER2'] = 0.05 # Bias towards 3-GO-2-STAY
+playerParameters1['ALTER3'] = 0.05 # Bias towards 3-GO-2-STAY
+playerParameters1['ALTER4'] = 0.05 # Bias towards 3-GO-2-STAY
+playerParameters1['ALTER5'] = 0.05 # Bias towards 3-GO-2-STAY
 playerParameters1['alpha'] = 100 # How much the focal region augments attractiveness
 playerParameters1['beta'] = 30 # Amplitude of the WSLS sigmoid function
 playerParameters1['gamma'] = 31 # Position of the WSLS sigmoid function
@@ -31,15 +34,7 @@ playerParameters1['delta'] = 100 # How much the added FRA similarities augments 
 playerParameters1['epsilon'] = 30 # Amplitude of the FRA sigmoid function
 playerParameters1['zeta'] = 0.7 # Position of the FRA sigmoid function
 
-playerParameters2 =  {'ALL': 0.1} # Bias towards ALL
-playerParameters2['NOTHING'] = 0.1 # Bias towards NOTHING
-playerParameters2['3-GO-2-STAY'] = 0.1 # Bias towards 3-GO-2-STAY
-playerParameters2['alpha'] = 100 # How much the focal region augments attractiveness
-playerParameters2['beta'] = 30 # Amplitude of the WSLS sigmoid function
-playerParameters2['gamma'] = 31 # Position of the WSLS sigmoid function
-playerParameters2['delta'] = 100 # How much the added FRA similarities augments attractiveness
-playerParameters2['epsilon'] = 30 # Amplitude of the FRA sigmoid function
-playerParameters2['zeta'] = 0.7 # Position of the FRA sigmoid function
+playerParameters2 = playerParameters1
 
 print("****************************")
 print('Starting simulation')
@@ -48,7 +43,11 @@ print('--- Model parameters ----')
 print('--- Player 1 ----')
 print('Bias towards ALL: ', playerParameters1['ALL'])
 print('Bias towards NOTHING: ', playerParameters1['NOTHING'])
-print('Bias towards 3-GO-2-STAY: ', playerParameters1['3-GO-2-STAY'])
+print('Bias towards ALTER1: ', playerParameters1['ALTER1'])
+print('Bias towards ALTER2: ', playerParameters1['ALTER2'])
+print('Bias towards ALTER3: ', playerParameters1['ALTER3'])
+print('Bias towards ALTER4: ', playerParameters1['ALTER4'])
+print('Bias towards ALTER5: ', playerParameters1['ALTER5'])
 print('alpha: ', playerParameters1['alpha'])
 print('beta: ', playerParameters1['beta'])
 print('gamma: ', playerParameters1['gamma'])
@@ -59,7 +58,11 @@ print("\n")
 print('--- Player 2 ----')
 print('Bias towards ALL: ', playerParameters2['ALL'])
 print('Bias towards NOTHING: ', playerParameters2['NOTHING'])
-print('Bias towards 3-GO-2-STAY: ', playerParameters2['3-GO-2-STAY'])
+print('Bias towards ALTER1: ', playerParameters2['ALTER1'])
+print('Bias towards ALTER2: ', playerParameters2['ALTER2'])
+print('Bias towards ALTER3: ', playerParameters2['ALTER3'])
+print('Bias towards ALTER4: ', playerParameters2['ALTER4'])
+print('Bias towards ALTER5: ', playerParameters2['ALTER5'])
 print('alpha: ', playerParameters2['alpha'])
 print('beta: ', playerParameters2['beta'])
 print('gamma: ', playerParameters2['gamma'])
@@ -74,7 +77,7 @@ print('Number of rounds: ', gameParameters[2])
 print('Number of groups: ', gameParameters[3])
 print("\n")
 
-E = DL.Experiment(gameParameters, [playerParameters1, playerParameters2], non_shaky_hand=non_shaky_hand)
+E = DL.Experiment(gameParameters, [playerParameters1, playerParameters2])
 if TO_FILE:
         with open('temp.csv', 'w') as dfile:
             head = 'index,Group,Round,Player,Decision,Score,Strategy\n'
